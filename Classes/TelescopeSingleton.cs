74ce5d29e20180fedafd45fcb1efc679ef48feb7
paulmrest <paulmrest@gmail.com>
2020-08-26 19:14:45 -0700

    Demo creation

---
+++ Classes/TelescopeSingleton.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace SingletonDemo.Classes
+{
+    public class TelescopeSingleton
+    {
+        //Necessary Singleton properties
+        private static TelescopeSingleton _telescope { get; set; }
+
+        private static object SyncLock = new Object();
+
+        //Telescope properties
+        private double RightAscension { get; set; }
+
+        private double Declination { get; set; }
+
+
+        //Private constructor
+        private TelescopeSingleton()
+        {
+            RightAscension = 0.0f;
+            Declination = 0.0f;
+        }
+
+        //
+        public static TelescopeSingleton GetInstance()
+        {
+            if (_telescope is null)
+            {
+                lock (SyncLock)
+                {
+                    if (_telescope is null)
+                    {
+                        _telescope = new TelescopeSingleton();
+                    }
+                }
+            }
+            return _telescope;
+        }
+
+        public void Slew(double RA, double dec)
+        {
+            RightAscension = RA;
+            Declination = dec;
+        }
+
+        public override string ToString()
+        {
+            return $"\r\nRight Ascension: {String.Format("{0:0.00}", RightAscension)}\r\nDeclination: {String.Format("{0:0.00}", Declination)}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using SingletonDemo.Classes;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Iverson Observatory! The obvervatory where the answer is always yes but no, or no but yes.");
+            Console.WriteLine();
+
+            Console.WriteLine("First let's get a telescope instance...");
+            TelescopeSingleton telescope = TelescopeSingleton.GetInstance();
+            Console.WriteLine($"Our telescope's initial position is: {telescope}");
+            
+            Console.WriteLine();
+
+            Console.WriteLine("Let's slew our telescope to right ascension 90 degrees, and declination 45 degrees.");
+            telescope.Slew(90.0f, 45.0f);
+            Console.WriteLine($"Our telescope's position is now: {telescope}");
+            Console.WriteLine();
+
+            Console.WriteLine("Next we'll get a \"separate\" (wink wink) instance of our telescope...");
+            TelescopeSingleton telescopeAgain = TelescopeSingleton.GetInstance();
+            Console.WriteLine($"Our \"new\" telescope has a position of: {telescopeAgain}");
+        }
+    }
+}
